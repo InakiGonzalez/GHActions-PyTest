009bd8b37aacd63d69446fe33cc493c6a6fc8c31
Iñaki GM <inakiglzm@outlook.com>
2025-06-10 16:39:54 -0600

    fixed some other things on test file

--- test_area_triangulo.py
+++ test_area_triangulo.py
@@ -4,4 +4,4 @@ def test_area_triangulo_correcto():
     assert calcular_area(10, 5) == 25
 
 def test_area_triangulo_incorrecto():
-    assert calcular_area(10, 3) == 20
+    assert calcular_area(10, 3) != 20
