01f50eee577ab2efcf98ce5354c6658eb7beaabb
Iñaki GM <inakiglzm@outlook.com>
2025-06-10 16:36:12 -0600

    test

--- test_area_triangulo.py
+++ test_area_triangulo.py
@@ -4,4 +4,4 @@ def test_area_triangulo_correcto():
     assert area_triangulo.calcular_area(10, 5) == 25
 
 def test_area_triangulo_incorrecto():
-    assert area_triangulo.calcular_area(10, 5) == 20
+    assert area_triangulo.calcular_area(10, 3) == 20
