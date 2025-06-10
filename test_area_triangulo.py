bbc299950217946f71437b2c3e2a5851751e1422
Iñaki GM <inakiglzm@outlook.com>
2025-06-10 16:39:14 -0600

    fixed some other things on test file

--- test_area_triangulo.py
+++ test_area_triangulo.py
@@ -1,7 +1,7 @@
 from app import calcular_area
 
 def test_area_triangulo_correcto():
-    assert area_triangulo.calcular_area(10, 5) == 25
+    assert calcular_area(10, 5) == 25
 
 def test_area_triangulo_incorrecto():
-    assert area_triangulo.calcular_area(10, 3) == 20
+    assert calcular_area(10, 3) == 20
